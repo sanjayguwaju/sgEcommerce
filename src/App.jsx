f6ab9f1db3b20e98695ddc69152c4c0d2e40a88e
sanjayguwaju <sanjayguwaju@gmail.com>
2023-07-07 09:38:05 +0545

    fixed: imported footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "../src/components/Navbar/Navbar"
 import Header from "../src/components/Header/Header"
+import Footer from "../src/components/Footer/Footer"
 
 const App = () => {
   return 
   <>
     <Navbar/>
     <Header/>
+    <Footer/>
   </>;
 };
 
