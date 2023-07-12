b2e6798f96b6f02669c3b50f827feb2b7b3aa6ad
sanjayguwaju <sanjayguwaju@gmail.com>
2023-07-12 09:44:18 +0545

    fix: app css imported

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Navbar from "../src/components/Navbar/Navbar"
 import Header from "../src/components/Header/Header"
 import Footer from "../src/components/Footer/Footer"
 
+import "./App.css"
+
 const App = () => {
   return 
   <>
